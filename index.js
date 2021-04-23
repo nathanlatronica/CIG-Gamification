9cf24e4cfeefa8a5c5a4f9dabe064a2061cd8a35
nathanlatronica <nlatronica@csumb.edu>
2021-04-22 17:09:33 -0700

    some database stuff

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const session = require('express-session');
+const mysql = require("mysql");
 const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 8000;
@@ -14,6 +15,50 @@ app.use(bodyParser.json())
 app.get("/",  function(req, res){    
     res.render("test");
   });
+
+app.get("/userInfo",  function(req, res){    
+  res.render("userInfo");
+});  
+
+  //This allows us to connect to the database it uses heroku its called JawsDB
+function dbConnection(){
+  let connection = mysql.createConnection({
+      host: 'xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      user: 'h9q8zn8q3hq8it7e',
+      password: 'dz7edod4ywrjeea9',
+      database: 'wi1mn51lp91tvquw'
+    })
+
+    return connection
+}
+
+
+function dbSetup() {
+  let connection = dbConnection();
+  
+  connection.connect()
+    /* 
+    This is were we create the tables to store information
+    Not 100% sure yet what we are gonna store so its kinda blank
+    Could be multiple choice questions... There is  name and email .. etc
+    */
+  var createUserInfo = `CREATE TABLE IF NOT EXISTS UserInfo
+                      (id int NOT NULL AUTO_INCREMENT,
+                      name varchar(60) NOT NULL,
+                      email varchar(75) NOT NULL,
+                      PRIMARY KEY(id)
+                      );`
+  connection.query(createUserInfo, function (err, rows, fields) {
+      if (err) {
+          throw err
+      }
+  })
+  
+  
+  connection.end()
+}
+  
+dbSetup()
 
 //starting server
 app.listen(port, () => {
